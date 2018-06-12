d629cdbdadc71edd782014cc58189b4b81d82304
Dogukan Ermis <D-Ermis@users.noreply.github.com>
2018-06-12 21:01:53 +0200

    Updated to v1.1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "resource.h"
-#include <stdlib.h>
 
 UINT WM_TASKBAR = 0;
 HWND Hwnd;
@@ -11,7 +10,7 @@ char szClassName[ ] = "Night Light";
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 void InitNotifyIconData();
 void ChangeColorTemp(int,int);
-
+void SetColorTempBasedOnTime();
 
 int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow)
 {
@@ -36,8 +35,13 @@ int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpsz
     // Initialize NOTIFYICONDATA structure and show system tray icon.
     InitNotifyIconData();
 
-    //Change Color Temperature to 2600K by default
-    ChangeColorTemp(165,79);
+    // Check local time every 5 min and set color temperature accordingly
+    // Default settings:
+    // Day: 4200K
+    // Evening: 3400K
+    // Night: 2300K
+    SetTimer(Hwnd, IDT_TIMER1, 300000, (TIMERPROC) NULL);
+    SetColorTempBasedOnTime();
 
     MSG msg;
     while (GetMessage (&msg, NULL, 0, 0)){
@@ -52,13 +56,6 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     switch (message){
     case WM_CREATE:
         Hmenu = CreatePopupMenu();
-        AppendMenu(Hmenu, MF_STRING, ID_TRAY_1900K,  TEXT("1900K: Candle"));
-        AppendMenu(Hmenu, MF_STRING, ID_TRAY_2300K,  TEXT("2300K: Dim Incandescent"));
-        AppendMenu(Hmenu, MF_STRING, ID_TRAY_2600K,  TEXT("2600K: Incandescent"));
-        AppendMenu(Hmenu, MF_STRING, ID_TRAY_3400K,  TEXT("3400K: Halogen"));
-        AppendMenu(Hmenu, MF_STRING, ID_TRAY_4200K,  TEXT("4200K: Fluorescent"));
-        AppendMenu(Hmenu, MF_STRING, ID_TRAY_5500K,  TEXT("5500K: Sunlight"));
-        AppendMenu(Hmenu, MF_STRING, ID_TRAY_6500K,  TEXT("6500K: Daylight"));
         AppendMenu(Hmenu, MF_STRING, ID_TRAY_EXIT,  TEXT("Exit"));
         break;
 
@@ -66,7 +63,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         if (lParam == WM_RBUTTONDOWN){
             POINT curPoint;
             GetCursorPos(&curPoint);
-			SetForegroundWindow(Hwnd);
+			SetForegroundWindow(hwnd);
             UINT clicked = TrackPopupMenu(Hmenu,TPM_RETURNCMD | TPM_NONOTIFY,curPoint.x,curPoint.y,0,hwnd,NULL);
             SendMessage(hwnd, WM_NULL, 0, 0);
 
@@ -75,23 +72,17 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                 ChangeColorTemp(249,253);
                 DestroyWindow(hwnd);
             }
-            else if (clicked == ID_TRAY_6500K)
-                ChangeColorTemp(249,253);
-            else if (clicked == ID_TRAY_5500K)
-                ChangeColorTemp(236,224);
-            else if (clicked == ID_TRAY_4200K)
-                ChangeColorTemp(213,173);
-            else if (clicked == ID_TRAY_3400K)
-                ChangeColorTemp(193,132);
-            else if (clicked == ID_TRAY_2600K)
-                ChangeColorTemp(165,79);
-            else if (clicked == ID_TRAY_2300K)
-                ChangeColorTemp(152,54);
-            else if (clicked == ID_TRAY_1900K)
-                ChangeColorTemp(131,0);
         }
     }
     break;
+
+    case WM_TIMER:
+    switch (wParam)
+    {
+        case IDT_TIMER1:
+            SetColorTempBasedOnTime();
+            return 0;
+    }
 
     case WM_NCHITTEST:{
        UINT uHitTest = DefWindowProc(hwnd, WM_NCHITTEST, wParam, lParam);
@@ -101,6 +92,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             return uHitTest;
     }
     case WM_DESTROY:
+        KillTimer(hwnd, IDT_TIMER1);
         PostQuitMessage(0);
         break;
 
@@ -139,4 +131,16 @@ void ChangeColorTemp(int green, int blue)
     ::SetDeviceGammaRamp (hGammaDC, GammaArray);
     Sleep (100);
     ::ReleaseDC(NULL, hGammaDC);
+}
+
+void SetColorTempBasedOnTime()
+{
+    SYSTEMTIME lt;
+    GetLocalTime(&lt);
+    if(lt.wHour < 8 || lt.wHour > 21)
+        ChangeColorTemp(152,54); // 2300K
+    else if(lt.wHour > 8 && lt.wHour < 18)
+        ChangeColorTemp(213,173); // 4200K
+    else
+        ChangeColorTemp(193,132); // 3400K
 }
--- resource.h
+++ resource.h
@@ -4,11 +4,5 @@
 #define ICO1 101
 #define ID_TRAY_APP_ICON    1001
 #define ID_TRAY_EXIT        1002
-#define ID_TRAY_6500K       1004
-#define ID_TRAY_5500K       1005
-#define ID_TRAY_4200K       1006
-#define ID_TRAY_3400K       1007
-#define ID_TRAY_2600K       1008
-#define ID_TRAY_2300K       1009
-#define ID_TRAY_1900K       1010
+#define IDT_TIMER1          1003
 #define WM_SYSICON          (WM_USER + 1)
